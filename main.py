ee286046eb2d694b9255e22edce84d81b3fe02f7
Ji Hoon Lee <jihoon.lee@morningstar.com>
2023-05-06 10:41:42 -0400

    Added queue for each domain.

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import threading
 import utils
 
 
-SCRAP_QUEUE = queue.Queue()
 WRITE_DB_QUEUE = queue.Queue()
 NUM_THREADS_PER_DOMAIN = 5
 
@@ -14,10 +13,12 @@ NUM_THREADS_PER_DOMAIN = 5
 def _write_to_db(num_total_workers):
 	with database.connect() as con:
 		cur = con.cursor()
-		while num_total_workers > 0:
+		current_workers = num_total_workers
+		while current_workers > 0:
 			item = WRITE_DB_QUEUE.get()
 			if item is None:
-				num_total_workers -= 1
+				current_workers -= 1
+				print(f"Worker finished {current_workers}/{num_total_workers}")
 				continue
 			domain_id, href, title, body, unix_timestamp = item
 			cur.execute("SELECT ifnull(max(id)+1, 0) FROM news")
@@ -26,16 +27,15 @@ def _write_to_db(num_total_workers):
 			con.commit()
 
 
-def _bfs(domain_id, domain_url, domain_news_pattern, visited, depth):
-	global SCRAP_QUEUE
+def _bfs(domain_id, domain_url, domain_news_pattern, scrap_queue, visited, depth):
 	global WRITE_DB_QUEUE
 
-	while SCRAP_QUEUE:
+	while scrap_queue:
 		if depth == 0:
 			WRITE_DB_QUEUE.put(None)
 			break
 		try:
-			node_href, node_is_last = SCRAP_QUEUE.get()
+			node_href, node_is_last = scrap_queue.get()
 			print(f"Scraping: {node_href} Depth: {depth}")
 			node_raw_html = html_parser.raw_html(node_href)
 			if utils.is_news(domain_news_pattern, node_href):
@@ -50,12 +50,12 @@ def _bfs(domain_id, domain_url, domain_news_pattern, visited, depth):
 
 			for idx, item in enumerate(node_graph):
 				if not item in visited:
-					SCRAP_QUEUE.put((item, False))
+					scrap_queue.put((item, False))
 					visited.add(item)
 				else:
 					if (idx == len_node_graph - 1) and node_is_last:
-						new_last, _ = SCRAP_QUEUE.get(-1)
-						SCRAP_QUEUE.put((new_last, True))
+						new_last, _ = scrap_queue.get(-1)
+						scrap_queue.put((new_last, True))
 		except Exception as e:
 			print(e)
 			continue
@@ -78,8 +78,9 @@ def main():
 			domain_news_pattern_str, = cur.fetchone()
 			domain_news_pattern = re.compile(rf"{domain_news_pattern_str}")
 
-			depth = 2
+			depth = 1
 			visited = set()
+			scrap_queue = queue.Queue()
 			visited.add(domain_url)
 
 			domain_raw_html = html_parser.raw_html(domain_url)
@@ -91,10 +92,10 @@ def main():
 				if idx == len_domain_graph - 1:
 					is_last = True
 				visited.add(href)
-				SCRAP_QUEUE.put((href, is_last))
+				scrap_queue.put((href, is_last))
 
 			for i in range(NUM_THREADS_PER_DOMAIN):
-				t = threading.Thread(target=_bfs, args=(domain_id, domain_url, domain_news_pattern, visited, depth))
+				t = threading.Thread(target=_bfs, args=(domain_id, domain_url, domain_news_pattern, scrap_queue, visited, depth))
 				scrap_threads.append(t)
 				t.start()
 

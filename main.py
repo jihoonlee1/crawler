485c431bd195e79b7342b3a15e0cfe1655b216fd
Ji Hoon Lee <jihoon.lee@morningstar.com>
2023-05-06 04:39:04 -0400

    Separated for modulartiy.

--- main.py
+++ main.py
@@ -5,41 +5,35 @@ import queue
 import threading
 import goose3
 import re
+import database
 
 
 SCRAP_QUEUE = queue.Queue()
+WRITE_DB_QUEUE = queue.Queue()
 NUM_SCRAP_THREADS = 3
 VISITED = set()
 LOCK = threading.Lock()
 DEPTH = 1
 NEWS_PATTERN = re.compile(r"https:\/\/www\.cnn\.com\/(2[0-9]{3})\/([0-9]{2})\/([0-9]{2})\/.+$")
+ROOT = "https://www.cnn.com/"
 
 
-def _get_raw_html(root):
-	r = requests.get(root)
+def _raw_html(href):
+	r = requests.get(href)
 	return r.text
 
 
-def _get_article(raw_html):
-	with goose3.Goose() as goose:
-		return goose.extract(raw_html=raw_html)
-
-
-def _get_html_doc(raw_html):
+def _html_doc(raw_html):
 	html_doc = bs4.BeautifulSoup(raw_html, "html.parser")
 	return html_doc
 
 
-def _get_all_links(html_doc):
+def _all_links_inc_dup(html_doc):
 	links = html_doc.find_all("a", {"href": True})
 	return links
 
 
-def is_news(href):
-	return NEWS_PATTERN.search(href)
-
-
-def _get_relevant_links(links, root_netloc):
+def _relevant_links(links, root_netloc=ROOT):
 	result = []
 	for item in links:
 		temp = urllib.parse.urlparse(item["href"])
@@ -51,7 +45,7 @@ def _get_relevant_links(links, root_netloc):
 	return result
 
 
-def _get_final_hrefs(relevant_links, root_netloc):
+def _hrefs(relevant_links, root_netloc=ROOT):
 	result = set()
 	for item in relevant_links:
 		temp = urllib.parse.urlparse(item["href"])
@@ -61,33 +55,43 @@ def _get_final_hrefs(relevant_links, root_netloc):
 	return result
 
 
+def _node_hrefs(node_raw_html):
+	node_html_doc = _html_doc(node_raw_html)
+	all_links_inc_dup = _all_links_inc_dup(node_html_doc)
+	relevant_links = _relevant_links(all_links_inc_dup)
+	return _hrefs(relevant_links)
+
+
 def _article(raw_html):
 	with goose3.Goose() as goose:
 		article = goose.extract(raw_html=raw_html)
 		return (article.title, article.body, article.publish_date)
 
 
-def _hrefs(root):
-	root_netloc = urllib.parse.urlparse(root).netloc
-	raw_html = _get_raw_html(root)
-	html_doc = _get_html_doc(raw_html)
-	links = _get_all_links(html_doc)
-	relevant_links = _get_relevant_links(links, root_netloc)
-	final_hrefs = _get_final_hrefs(relevant_links, root_netloc)
-	return final_hrefs
+def _is_news(href):
+	test = NEWS_PATTERN.search(href)
+	if test is not None:
+		return True
+	return False
 
 
 def _bfs():
 	global SCRAP_QUEUE
+	global WRITE_DB_QUEUE
 	global VISITED
 	global DEPTH
 
 	while SCRAP_QUEUE:
 		if DEPTH == 0:
 			break
-		root, root_is_last = SCRAP_QUEUE.get()
+		node_href, node_is_last = SCRAP_QUEUE.get()
+		node_raw_html = _raw_html(node_href)
+		if _is_news(root):
+			WRITE_DB_QUEUE.append(_article(node_raw_html))
 		print(root, root_is_last)
-		graph = [href for href in _hrefs(root)]
+		graph = [href for href in _node_hrefs(node_raw_html)]
+
+
 		len_graph = len(graph)
 		if root_is_last:
 			with LOCK:
@@ -105,13 +109,13 @@ def _bfs():
 
 
 def main():
-	root = "https://www.cnn.com/"
-	VISITED.add(root)
-	graph = [href for href in _hrefs(root)]
+	VISITED.add(ROOT)
+	root_raw_html = _raw_html(ROOT)
+	root_graph = [href for href in _node_hrefs(root_raw_html)]
+	len_graph = len(root_graph)
 	scrap_threads = []
-	len_graph = len(graph)
 
-	for idx, item in enumerate(graph):
+	for idx, item in enumerate(root_graph):
 		is_last = False
 		if idx == len_graph - 1:
 			is_last = True

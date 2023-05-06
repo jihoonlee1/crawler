d668f35430aa97ae41faadabd0b71b391badf3e4
Ji Hoon Lee <jihoon.lee@morningstar.com>
2023-05-06 04:22:45 -0400

    Added database schema.

--- database.py
+++ database.py
@@ -1,1 +1,30 @@
+import contextlib
 import sqlite3
+
+
+STATEMENTS = [
+"""
+CREATE TABLE articles(
+	id           INTEGER NOT NULL PRIMARY KEY,
+	url          TEXT    NOT NULL,
+	title        TEXT,
+	body         TEXT,
+	publish_date TEXT    NOT NULL
+)
+"""
+]
+
+
+def connect(database="database.sqlite", mode="rw"):
+	return contextlib.closing(sqlite3.connect(f"file:{database}?mode={mode}", uri=True))
+
+
+def main():
+	with connect(mode="rwc") as con:
+		cur = con.cursor()
+		for st in STATEMENTS:
+			cur.execute(st)
+
+
+if __name__ == "__main__":
+	main()
